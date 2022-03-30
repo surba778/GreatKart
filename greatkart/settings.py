1585fb2789582f9560f1972c53cdd1d2b1e075d5
surba778 <surba778@gmail.com>
2022-03-30 11:24:12 +0000

    Updated homepage

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -147,6 +148,11 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'greatkart/static',
 ]
+
+# media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
