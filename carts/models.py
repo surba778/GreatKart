0cd72490ef7c7073b8327dcea6f60f056f0e4d88
surba778 <surba778@gmail.com>
2022-04-04 18:32:47 +0000

    Updated cart

--- carts/models.py
+++ carts/models.py
@@ -22,4 +22,7 @@ class CartItem(models.Model):
     def sub_total(self):
         return self.product.price * self.quantity
 
+    def __str__(self):
+        return self.product
+
     
--- carts/views.py
+++ carts/views.py
@@ -35,8 +35,44 @@ def add_cart(request, product_id):
             quantity= 1,
             cart= cart,
     )
-    cart_item.save()
+        cart_item.save()
     return redirect('cart')
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def remove_cart(request, product_id, cart_item_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+            for cart_item in cart_items:
+                total += (cart_item.product.price * cart_item.quantity)
+                quantity += cart_item.quantity
+            tax = (2 * total)/100
+            grand_total = total + tax
+        
+    except ObjectDoesNotExist:
+        pass #just ignore
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        
+    }
+    return render(request, 'store/cart.html', context)
