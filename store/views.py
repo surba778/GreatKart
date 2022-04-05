cf545738f053fc0a805fcb3a460f6d974a231244
surba778 <surba778@gmail.com>
2022-04-05 18:59:39 +0000

    Cart is updated

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
 
+from carts.views import _cart_id
+from django.http import HttpResponse
 def store(request, category_slug=None):
     categories = None
     products = None
@@ -30,6 +33,8 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+       
     except Exception as e:
         raise e
 
