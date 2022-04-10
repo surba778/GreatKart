fea116904dab651d8e16e4a07d1452ef16b878c1
surba778 <surba778@gmail.com>
2022-04-10 17:45:50 +0000

    Updated search

--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from carts.models import CartItem
 
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
 
 def store(request, category_slug=None):
     categories = None
@@ -42,5 +43,30 @@ def product_detail(request, category_slug, product_slug):
 
     context = {
         'single_product': single_product,
+        'in_cart'       : in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
