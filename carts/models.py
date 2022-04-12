5680f1cc4fa56898916a8b1eccba21329f989a6c
surba778 <surba778@gmail.com>
2022-04-12 14:35:12 +0000

    cart is updated

--- carts/models.py
+++ carts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from store.models import Product
-
+from store.models import Product, Variation
+from accounts.models import Account
 
 
 # Create your models here.
@@ -14,7 +14,9 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(Variation, blank=True)
     cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
@@ -22,7 +24,5 @@ class CartItem(models.Model):
     def sub_total(self):
         return self.product.price * self.quantity
 
-    def __str__(self):
+    def __unicode__(self):
         return self.product
-
-    
