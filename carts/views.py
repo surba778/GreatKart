9cf4d95e41dac5b0a34c63f5a6b638b151779d32
surba778 <surba778@gmail.com>
2022-04-16 15:45:58 +0000

    product_detail.html is updated

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product, Variation
+from store.models import Product
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
