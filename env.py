9046ea2dda22c5ae442ff0a36c81d6c0f1bc7ee2
surba778 <surba778@gmail.com>
2022-04-19 18:25:36 +0000

    testing env variables

--- env.py
+++ env.py
@@ -3,3 +3,6 @@ import os
 os.environ["DATABASE_URL"] = "postgres://wfdrlrzoptcdop:34df96a4caa128e939fc65fd70f5f00c127e70c1d04196ccfe2bd05e921cc01a@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/df5o5stemguttm"
 os.environ["SECRET_KEY"] = "randomSecretKey2023"
 os.environ["DEVELOPMENT"] = "True"
+os.environ["USE_AWS"] = "True"
+os.environ["AWS_ACCESS_KEY_ID"] = "AKIAYKBGXAKPX2PLWMMO"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
