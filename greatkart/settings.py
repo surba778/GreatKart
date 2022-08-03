782511d2f3a894180e48fc22ff2d225cd9c34cfc
surba778 <surba778@gmail.com>
2022-08-03 17:52:32 +0000

    Add changes in env.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -208,13 +208,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='anpeffqpefggjvgh')
 # EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_USE_TLS = True
-#EMAIL_PORT = 587
-#EMAIL_HOST_USER = 'acies.venkadesh@gmail.com'
-#EMAIL_HOST_PASSWORD = 'anpeffqpefggjvgh'
+
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
