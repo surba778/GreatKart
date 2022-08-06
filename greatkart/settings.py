02d747b4cdaeb607941c779ea543f95d85d34ecf
surba778 <surba778@gmail.com>
2022-08-06 11:11:18 +0000

    Added env.py file

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -117,17 +117,10 @@ WSGI_APPLICATION = 'greatkart.wsgi.application'
 AUTH_USER_MODEL = 'accounts.Account'
 
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
+DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+
 
 
 # Password validation
