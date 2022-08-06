6d7ed4567568b4c03128a36ebcc42dde790d5814
surba778 <surba778@gmail.com>
2022-08-06 11:12:51 +0000

    change in setting.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -117,10 +117,17 @@ WSGI_APPLICATION = 'greatkart.wsgi.application'
 AUTH_USER_MODEL = 'accounts.Account'
 
 
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
