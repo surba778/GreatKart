0c4c23f1ba23ed3ebb6b10f1327d879d31585ec1
surba778 <surba778@gmail.com>
2022-08-12 16:48:31 +0000

    Added gitignore

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ["DATABASE_URL"] = "postgres://osvbvolgdfavng:04cc67810252c887f3ee6cea284f92df9020df9b6e19704315d738fb957a16b5@ec2-54-75-26-218.eu-west-1.compute.amazonaws.com:5432/d95h3rkrjblsmc"
-os.environ["SECRET_KEY"] = "R2IQsZl2Lw"
+os.environ["SECRET_KEY"] = "R2IQsZl2LL"
 os.environ["DEVELOPMENT"] = "True"
 os.environ["USE_AWS"] = "True"
 os.environ["DISABLE_COLLECTSTATIC"] = "1"
