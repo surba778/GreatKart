517ef56fa4a7f62dc660d7cef675e6fec078f3c6
surba778 <surba778@gmail.com>
2022-08-12 18:42:56 +0000

    Change made in setting.py file

--- env.py
+++ env.py
@@ -1,8 +1,6 @@
 import os
 
 os.environ["DATABASE_URL"] = "postgres://osvbvolgdfavng:04cc67810252c887f3ee6cea284f92df9020df9b6e19704315d738fb957a16b5@ec2-54-75-26-218.eu-west-1.compute.amazonaws.com:5432/d95h3rkrjblsmc"
-os.environ["SECRET_KEY"] = "R2IQsZl2LL"
-os.environ["DEVELOPMENT"] = "True"
+os.environ["SECRET_KEY"] = "i+z0m!4^6su8gt#334j79vdd(-@kk1qd^wm*+mgn1jit=ph1-!"
 os.environ["USE_AWS"] = "True"
-os.environ["DISABLE_COLLECTSTATIC"] = "1"
-os.environ["DEVELOPMENT"] = True
+os.environ["DEVELOPMENT"] = "True"
