75b8fd2b6a01397f2496c6e00f50b6bf32c3daae
surba778 <surba778@gmail.com>
2022-08-13 10:05:10 +0000

    Removed env.py file

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,10 +5,8 @@ from django.utils.html import format_html
 
 # Register your models here.
 
-
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name',
-                    'username', 'last_login', 'date_joined')
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
     list_display_links = ('email', 'first_name', 'last_name')
     readonly_fields = ('last_login', 'date_joined')
     ordering = ('-date_joined',)
@@ -17,13 +15,11 @@ class AccountAdmin(UserAdmin):
     list_filter = ()
     fieldsets = ()
 
-
 class UserProfileAdmin(admin.ModelAdmin):
     def thumbnail(self, object):
         return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
     thumbnail.short_description = 'Profile Picture'
     list_display = ('thumbnail', 'user', 'city', 'state', 'country')
-
 
 admin.site.register(Account, AccountAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -6,7 +6,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-from pathlib import Path
+
 import os
 import dj_database_url
 if os.path.exists("env.py"):
