1c63d8bc166f71c40f017d12f13d17f147c4cac5
surba778 <surba778@gmail.com>
2022-09-27 13:17:23 +0000

    Added main page header

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -6,7 +6,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include('home.urls')),
-    path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 
