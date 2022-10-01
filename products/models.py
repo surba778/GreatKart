18b870482c56b048594fd4e2673e831fe8031507
surba778 <surba778@gmail.com>
2022-10-01 16:54:37 +0000

    Added sizes to product

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     class Meta:  
         verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -19,9 +20,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
-    is_limited_edition = models.BooleanField(
-        default=False, null=True, blank=True
-        )
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
