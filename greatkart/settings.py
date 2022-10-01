01551725c3b65583f7faa549b21a899f36cee717
surba778 <surba778@gmail.com>
2022-10-01 11:02:24 +0000

    Added shopping bag app, urls and templates

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'home',
     'contact',
     'products',
+    'bag',
 
     # Other
     'crispy_forms',
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -6,7 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-    path('allauth/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('contact/', include('contact.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
