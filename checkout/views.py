e4a994e345b4e219c48d916644ea83a731098e70
surba778 <surba778@gmail.com>
2022-10-02 17:11:59 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Ke1PhIptPaWBKYcjT3dSPmCbZflNX4RupbYTAOLYBtB1XevQz50riqqawazzHqKD5nQDw2jk9DJD8W7P6WLkxf700pjt1Vq7J',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
