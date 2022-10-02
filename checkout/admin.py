bb778f1829f751729789d120cad294778982add2
surba778 <surba778@gmail.com>
2022-10-02 15:54:39 +0000

    Added background image

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
-# Register your models here.
-
 
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
@@ -12,27 +10,20 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    # fields to be calc by model methods
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total', 'original_cart',
-                       'stripe_pid')
+                       'grand_total',)
 
-    # to specify order of fields in admin interface
-    fields = ('order_number', 'user_profile', 'date', 'full_name',
+    fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total', 'original_cart',
-              'stripe_pid')
+              'order_total', 'grand_total',)
 
-    # order list to show only few key items
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
                     'grand_total',)
 
-    # ordered by date in reverse chronological order
     ordering = ('-date',)
-
 
 admin.site.register(Order, OrderAdmin)
