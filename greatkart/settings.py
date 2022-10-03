49ab25683d4a101cff9e4caf737385efa0886ad9
surba778 <surba778@gmail.com>
2022-10-03 07:52:30 +0000

    Added checkout form data caching in payment intent

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -170,6 +170,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'USE_AWS' in os.environ:
     # Cache control
