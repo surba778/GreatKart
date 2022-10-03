1e24767c00a74a3b13fa423e5257eff7a4f5b23f
surba778 <surba778@gmail.com>
2022-10-03 18:43:47 +0000

    fix confirmation emails

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -172,6 +172,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'greatkart@example.com'
 
 if 'USE_AWS' in os.environ:
     # Cache control
