b21ff5f4e77acb89866e791e131c1112b462215e
surba778 <surba778@gmail.com>
2022-10-05 09:48:47 +0000

    fix change in 404

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import handler404
+
 
 urlpatterns = [
     path('', include('home.urls')),
@@ -13,3 +15,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'boutique_ado.views.handler404'
